15c4572fa302d0d6f71d0e0687b1d437e7c51dcf
Juedquipa <Juedquipa29@gmail.com>
2021-11-17 22:30:35 -0500

    Camilito

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -9,6 +9,7 @@ public class CameraControl : MonoBehaviour
     public Transform bg2;
     private float bgSize;
     public static bool pause = false;
+    private int startInt = 0;
 
 
     // Start is called before the first frame update
@@ -20,18 +21,26 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (pause == false)
+        if(startInt > 1500)
         {
-            // Camera
-            Vector3 newPos = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
-            transform.position = Vector3.Lerp(transform.position, newPos, 0.2f);
-
-            // Background
-            if(transform.position.y >= bg2.position.y)
+            if (pause == false)
             {
-                bg1.position = new Vector3(bg1.position.x, bg2.position.y+ bgSize, bg1.position.z);
-                SwitchBg();
+                // Camera
+                Vector3 newPos = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+                transform.position = Vector3.Lerp(transform.position, newPos, 0.2f);
+
+                // Background
+                if(transform.position.y >= bg2.position.y)
+                {
+                    bg1.position = new Vector3(bg1.position.x, bg2.position.y+ bgSize, bg1.position.z);
+                    SwitchBg();
+                }
             }
+        }
+        else
+        {
+            startInt = startInt + 1;
+            Debug.Log(startInt);
         }
     }
 
